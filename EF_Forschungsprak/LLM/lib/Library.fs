d4612c33b621f8ae4429af9de61eec8ee37973e3
EmFiliz <emre.filiz2000@googlemail.com>
2025-10-15 01:53:07 +0200

    Update 15.10.2025

--- EF_Forschungsprak/LLM/lib/Library.fs
+++ EF_Forschungsprak/LLM/lib/Library.fs
@@ -1,4 +1,4 @@
-﻿namespace FSharpLLM
+﻿namespace FsLLM
 
 open System
 open System.Text
@@ -7,43 +7,59 @@ open System.Threading.Tasks
 open Microsoft.Extensions.AI
 open OllamaSharp
 
-module LLM_Modal = 
-    // type Modal =
-    // | Gptoss
-    // | Deepseekr1
-    // | Gemma3
-    // | Embeddinggemma
-    // | Qwen3
-    // | Llama3
-    // | Phi3
-    // | Llava
-    // | Phi4
-    // | Gemma
+type Model =
+| Gpt_oss_20B
+| Deepseek_r1_8B
+| Gemma3_4B
+| Embeddinggemma_300M
+| Qwen3_8B
+| Deepseek_v3_1_671B
+| Llama3_1_8B
+| Nomic_embed_text_v1_5
+| Llama3_2_3B
+| Mistral_7B
+| Qwen2_5_7B
+| Phi3_3_8B
+| Llava
+| Phi4
+| Gemma with 
 
-    // let modalname m =
-    //     match m with
-    //     | Gptoss -> "gpt-oss:20b"
-    //     | Deepseekr1 -> "deepseek-r1:8b"
-    //     | Gemma3 -> "gemma3:4b"
-    //     | Embeddinggemma -> "embeddinggemma:300m"
-    //     | Qwen3 -> "qwen3:8b"
-    //     | Llama3 -> "llama3.1:8b"
-    //     | Phi3 -> "phi3:3.8b"
-    //     | Llava -> "llava:7b"
-    //     | Phi4 -> "phi4:14b"
-    //     | Gemma -> "gemma:7b"
+    static member getModelName m =
+        match m with
+        | Gptoss -> "gpt-oss:20b"
+        | Deepseekr1 -> "deepseek-r1:8b"
+        | Gemma3 -> "gemma3:4b"
+        | Embeddinggemma -> "embeddinggemma:300m"
+        | Qwen3 -> "qwen3:8b"
+        | Deepseek_v3_1_671B -> "deepseek-v3.1:671b"
+        | Llama3_1_8B -> "llama3.1:8b"
+        | Nomic_embed_text_v1_5 ->
+        | Llama3_2_3B -> 
+        | Mistral_7B -> 
+        | Qwen2_5_7B -> 
+        | Phi3_3_8B -> 
+        | Phi3 -> "phi3:3.8b"
+        | Llava -> "llava:7b"
+        | Phi4 -> "phi4:14b"
+        | Gemma -> "gemma:7b"
+    
+    static member initClient (m: Model) =
+        new OllamaApiClient(new Uri("http://localhost:11434/"), Model.getModelName m)
+        :> IChatClient
 
-    type ChatClient (model: string) =
-        let client : IChatClient = new OllamaApiClient(new Uri("http://localhost:11434/"), model)
+module Prompt =
+    let getResponse (client: IChatClient) (prompt: string) =
+        task{
+            let messages = [ChatMessage(ChatRole.User, prompt)]
+            let! response = client.GetResponseAsync(messages)
+            return response
+        }
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
 
-        member _.GetPrompt(prompt: string) =
-            let taskResult = task{
-                let messages = [ChatMessage(ChatRole.User, prompt)]
-                let! response = client.GetResponseAsync(messages :> IEnumerable<ChatMessage>)
-                Console.WriteLine("AI: \n" + response.ToString())
-            }
-            taskResult.GetAwaiter().GetResult()
-
+    let getResponseText (client: IChatClient) (prompt: string) = 
+        getResponse client prompt
+        |> _.ToString()
 
 // open System
 // open System.Text
